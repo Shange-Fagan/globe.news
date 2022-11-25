83303fc9fa723b428c3e8d9b2ec4802fd33fcca6
Shange-Fagan <shangefagansf@outlook.com>
2022-11-25 14:16:26 +0000

    srfr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), glsl()],
+  plugins: [glsl()],
 });
