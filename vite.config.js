4551ef8ceccfda377f959929b5846dd8b742320d
Shange-Fagan <shangefagansf@outlook.com>
2022-11-25 14:36:51 +0000

    fv

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
+import vitePluginString from "vite-plugin-string";
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [glsl(), vitePluginString()],
 });
